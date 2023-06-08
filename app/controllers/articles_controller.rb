2c8266596cb782c5a49796b542484dfc5466ffe7
Anshul Agrawal <anagrawal@bestpeers.com>
2023-06-08 11:15:09 +0530

    Added destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,12 +2,15 @@ class ArticlesController < ApplicationController
     def index 
       @articles = Article.all
     end
+
     def show
       @article = Article.find(params[:id])
     end
+
     def edit
       @article = Article.find(params[:id])
     end
+    
     def update 
       @article = Article.find(params[:id])
 
@@ -17,8 +20,15 @@ class ArticlesController < ApplicationController
         render :edit, status: :unprocessable_entity
       end
     end
+
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to root_path, status: :see_other
+    end 
+    
     private
-      def article_params
-        params.require(:article).permit(:title, :body)
-      end
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
