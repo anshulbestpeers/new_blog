db85694310418bb648c943614f6469c293451702
Anshul Agrawal <anagrawal@bestpeers.com>
2023-06-08 13:13:26 +0530

    Modified in comment model

--- db/migrate/20230608073013_add_article_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddArticleToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :comments, :article, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_08_073013) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_074106) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "body"
@@ -21,9 +21,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_073013) do
   create_table "comments", force: :cascade do |t|
     t.string "commentor"
     t.string "comment"
+    t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "article_id", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
