3f03e06b49a9286dc6cf22ecfe1d360002532f71
Anshul Agrawal <anagrawal@bestpeers.com>
2023-06-09 11:48:39 +0530

    Name field added in devise gem

---
+++ db/migrate/20230609060829_add_name_to_article_users.rb
@@ -0,0 +1,5 @@
+  class AddNameToArticleUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :article_users, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_08_104148) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_09_060829) do
   create_table "article_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_104148) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_article_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_article_users_on_reset_password_token", unique: true
   end
