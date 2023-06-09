d8f6ae84391832f6a68d8f73e050272320eee8f3
Anshul Agrawal <anagrawal@bestpeers.com>
2023-06-09 13:47:57 +0530

    Action mailer setup

--- app/models/article_user.rb
+++ app/models/article_user.rb
@@ -1,7 +1,14 @@
 class ArticleUser < ApplicationRecord
+  after_create :send_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
          :trackable
+
+  private
+  
+  def send_email
+    ArticleUserMailer.welcome_email(self).deliver_now
+  end
 end
